530b962e9f9da56b6a89f42a1a19cc03951c6896
Linugux <linugux@gmail.com>
2022-04-27 20:34:32 -0500

    27Abril

--- cowsay.php
+++ cowsay.php
@@ -1,22 +1,12 @@
- _________________________________________
-/ (literature) % Stop! There was first a  \
-| game of blindman's buff. Of course      |
-| there was. And I no more believe Topper |
-| was really blind than I believe he had  |
-| eyes in his boots. My opinion is, that  |
-| it was a done thing between him and     |
-| Scrooge's nephew; and that the Ghost of |
-| Christmas Present knew it. The way he   |
-| went after that plump sister in the     |
-| lace tucker, was an outrage on the      |
-\ credulity of human nature.              /
- -----------------------------------------
+ ________________________________________
+/ (fortunes) % Your best consolation is  \
+| the hope that the things you failed to |
+\ get weren't really worth having.       /
+ ----------------------------------------
      \
       \
-        ,__, |    | 
-        (oo)\|    |___
-        (__)\|    |   )\_
-             |    |_w |  \
-             |    |  ||   *
-
-             Cower....
+          oO)-.                       .-(Oo
+         /__  _\                     /_  __\
+         \  \(  |     ()~()         |  )/  /
+          \__|\ |    (-___-)        | /|__/
+          '  '--'    ==`-'==        '--'  '
--- fortuneCowsay.php
+++ fortuneCowsay.php
@@ -1,8 +1,4 @@
-(literature)
+(fortunes)
 %
-Stop!  There was first a game of blindman's buff.  Of course there was.
-And I no more believe Topper was really blind than I believe he had eyes
-in his boots.  My opinion is, that it was a done thing between him and
-Scrooge's nephew; and that the Ghost of Christmas Present knew it.  The
-way he went after that plump sister in the lace tucker, was an outrage
-on the credulity of human nature.
+Your best consolation is the hope that the things you failed to get weren't
+really worth having.
